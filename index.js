b8875456437f05fb54f766484799163c0591773d
T-Rekt <t.to.the.rekt@gmail.com>
2019-05-06 12:04:03 +0700

    Added register

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let UA = 'Reddit/Version 2.26.3/Build 226514/Android 8.0.0';
 let client_vendor_id = 'c879e75f-1d24-4a3b-8464-ce2167d7b974';
 
 request = request.defaults({
-  proxy: 'http://localhost:8080',
+  // proxy: 'http://localhost:8080',
   strictSSL: false,
   headers: {
     'User-Agent': UA
@@ -16,6 +16,15 @@ request = request.defaults({
 
 const secret = '8c7abaa5f905f70400c81bf3a1a101e75f7210104b1991f0cd5240aa80c4d99d';
 
+let errorHandling = (res) => {
+  if (res && res.json && res.json.errors && res.json.errors.length) {
+    let error_str = res.json.errors.map(err => err[1]).join(', ');
+    throw new Error(error_str);
+  }
+
+  return res;
+}
+
 let sign = (data, secret) => {
   let signed = hmacSHA256(data, secret);
 
@@ -23,7 +32,7 @@ let sign = (data, secret) => {
 }
 
 let login = (username, password) => {
-  let body = `rem=true&passwd=${password}&user=${username}&api_type=json`;
+  let body = `rem=true&passwd=${encodeURIComponent(password)}&user=${encodeURIComponent(username)}&api_type=json`;
 
   let ts = Date.now()/1000 | 0;
   let signed_result_data = sign(`Epoch:${ts}|User-Agent:${UA}|Client-Vendor-ID:${client_vendor_id}`, secret);
@@ -41,7 +50,8 @@ let login = (username, password) => {
     headers,
     body,
     json: true
-  });
+  })
+  .then(res => errorHandling(res));
 }
 
 let authorize = (cookie, modhash, client_id = 'ohXpoqrZYub1kg') => {
@@ -63,7 +73,8 @@ let authorize = (cookie, modhash, client_id = 'ohXpoqrZYub1kg') => {
     },
     simple: false,
     resolveWithFullResponse: true
-  });
+  })
+  .then(res => errorHandling(res));
 }
 
 let getAccessToken = (code, client_id = 'ohXpoqrZYub1kg', client_secret = 'nmlq8LvaC2ygotp8hcCEBW6NVqI') => {
@@ -78,7 +89,8 @@ let getAccessToken = (code, client_id = 'ohXpoqrZYub1kg', client_secret = 'nmlq8
       grant_type: 'authorization_code'
     }
   })
-  .then(res => JSON.parse(res));
+  .then(res => JSON.parse(res))
+  .then(res => errorHandling(res));
 }
 
 let loginAndGetAccessToken = (username, password) => {
@@ -95,7 +107,8 @@ let getMessages = (access_token) => {
     headers: {
       'Authorization': 'Bearer ' + access_token
     }
-  });
+  })
+  .then(res => errorHandling(res));
 }
 
 let sendMessage = (access_token, text, to, subject) => {
@@ -110,7 +123,8 @@ let sendMessage = (access_token, text, to, subject) => {
       api_type: 'json'
     }
   })
-  .then(res => JSON.parse(res));
+  .then(res => JSON.parse(res))
+  .then(res => errorHandling(res));
 }
 
 let replyMessage = (access_token, text, thing_id) => {
@@ -124,5 +138,31 @@ let replyMessage = (access_token, text, thing_id) => {
       api_type: 'json'
     }
   })
-  .then(res => JSON.parse(res));
-}      
+  .then(res => JSON.parse(res))
+  .then(res => errorHandling(res));
+}
+
+let register = (email, username, password) => {
+  let body = `email=${encodeURIComponent(email)}&passwd2=${encodeURIComponent(password)}&rem=true&passwd=${encodeURIComponent(password)}&user=${encodeURIComponent(username)}&api_type=json`;
+
+  let ts = Date.now()/1000 | 0;
+  let signed_result_data = sign(`Epoch:${ts}|User-Agent:${UA}|Client-Vendor-ID:${client_vendor_id}`, secret);
+  let signed_body_data = sign(`Epoch:${ts}|Body:${body}`, secret);
+
+  let headers = {
+    'User-Agent': UA,
+    'Client-Vendor-ID': client_vendor_id,
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'X-hmac-signed-body': `1:android:2:${ts}:${signed_body_data}`,
+    'X-hmac-signed-result': `1:android:2:${ts}:${signed_result_data}`
+  };
+
+  return request.post('https://www.reddit.com/api/v1/register', {
+    headers,
+    body,
+    json: true
+  })
+  .then(res => errorHandling(res));
+}
+
+module.exports = { loginAndGetAccessToken, getMessages, sendMessage, replyMessage, register }
